e430f8ecb9d9fa0d194010f44d020e2cb7dbd691
Sujoy Kumar Hens <sujoykumarhens@gmail.com>
2021-07-12 10:58:03 +0530

    ok

--- login.php
+++ login.php
@@ -1,8 +1,12 @@
-<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
 
+<!------ Include the above in your HEAD tag ---------->
+<html>
+	<head>
+	<link href="https://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="https://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+</head>
+<body>
 <div class="container">
     <div class="row">
     	<div class="col-md-4 col-md-offset-4">
@@ -35,3 +39,5 @@
 		</div>
 	</div>
 </div>
+</body>
+</html>
